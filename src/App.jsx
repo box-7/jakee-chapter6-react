12bf425df1c9f3f3134c2a692519e058c7e6dcf6
box-7 <hayainada1981@yahoo.co.jp>
2024-09-21 15:26:29 +0900

    コメント修正

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { ColorfulMessage } from "./components/ColorfulMessage";
 
 // exportすることで、他のファイルから参照できる
 export const App = () => {
-  console.log("App--------------------");
   // 1つ目の引数は変数、2つ目は関数、初期値はuseStateの引数
   // useStateは関数コンポーネントのコンポーネントの直下でしか呼べない
   const [num, setNum] = useState(0);
@@ -11,12 +10,11 @@ export const App = () => {
 
   const onClickCountup = () => {
     // setNumの状態が変わった時に、コンポーネントが最初から改めて読み込まれる
-    // 複数setNumがあると、最後の1回が読み込まれる
-    // setNum(num+1);
-    // setNum(num+1);
+      // 複数setNumがあると、最後の1回が読み込まれる
+      // setNum(num+1);
+      // setNum(num+1);
     // prevは現在のnum。関数を使うと、 想定通り2アップする
     setNum((prev) => prev + 1);
-    // setNum((prev) => prev + 1);
   };
 
   const onClickToggle = () => {
@@ -35,15 +33,16 @@ export const App = () => {
       }
     }
   }, [num]);
-
+  // 外側の{}: JSX内でJavaScriptを評価するために使います。これは、JSXの中で変数や関数を呼び出したり、計算結果を表示したいときに使います。
+  // return <p style={styleObj}>Hello, world!</p>;
+  // 内側の{}: オブジェクトや配列、値を渡す際に使うことができます
+  // return <p style={{ color: 'blue', backgroundColor: 'yellow' }}>Styled text</p>;
+  // オブジェクトの値は文字列で書く
   return (
     <>
-      {/* タグの中の{}はjsを書くということ、内側の{}は空のオブジェクト 
-    オブジェクトの値は文字列で書く*/}
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="green">元気です</ColorfulMessage>
-      {/* {console.log("hoge")} */}
       <button onClick={onClickCountup}>カウントアップ</button>
       <p>{num}</p>
       <button onClick={onClickToggle}>on/off</button>
@@ -53,7 +52,7 @@ export const App = () => {
 };
 
 // 再レンダリングされる条件
-// ステートが変わった時
-// 受け取っているpropsの値が変わった時
-// 親のコンポーネントが変わったら、子のコンポーネントも再レンダリングされる
+  // ステートが変わった時
+  // 受け取っているpropsの値が変わった時
+  // 親のコンポーネントが変わったら、子のコンポーネントも再レンダリングされる
 
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,9 +1,7 @@
 export const ColorfulMessage = (props) => {
-    console.log("ColorfulMessage--------------------");
   // ここで分割代入できる
   // export const ColorfulMessage = ({ color,children }) => {
   
-  //   console.log(props);
   const { color, children } = props;
   const contentStyleA = {
     // color: color, キーとバリューが同じ場合は1語でかける
